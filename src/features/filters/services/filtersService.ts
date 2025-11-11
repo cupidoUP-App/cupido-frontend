3840856d38a7e48c0849461f248cc3a16b8cc713
CupidoUP <cupidoup1@gmail.com>
2025-11-10 23:19:48 -0500

    Update filtersService.ts

--- src/features/filters/services/filtersService.ts
+++ src/features/filters/services/filtersService.ts
@@ -1,6 +1,7 @@
 // src/features/filters/services/filtersService.ts
 
-const API_BASE_URL = 'http://localhost:8000/api/v1';
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL;
 
 export interface UserFilters {
   id?: number;
