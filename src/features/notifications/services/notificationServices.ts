a355db024fab2513c19957d46cafd559205730d1
CupidoUP <cupidoup1@gmail.com>
2025-12-04 00:10:05 -0500

    Update notificationServices.ts

--- src/features/notifications/services/notificationServices.ts
+++ src/features/notifications/services/notificationServices.ts
@@ -37,7 +37,7 @@ const getUserIdFromToken = (): string | null => {
 };
 
 const VITE_API_BASE = import.meta.env.VITE_API_BASE_URL;  
-let API_BASE_URL = `${VITE_API_BASE}/api/v1/notificaciones/`;
+let API_BASE_URL = `${VITE_API_BASE}/notificaciones/`;
 
 export const NotificationsServices = {
     async getNotifications(): Promise<AppNotification[]> {
